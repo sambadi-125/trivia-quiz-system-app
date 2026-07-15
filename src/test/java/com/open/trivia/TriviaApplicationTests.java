92aa928721d9ef736ca2a22722af9643a8befb71
Magomed Abubakarov <magomed.abubakarov@gmail.com>
2026-07-15 19:10:19 +0200

    init: Trivia Quiz Springboot application

---
+++ src/test/java/com/open/trivia/TriviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.trivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
